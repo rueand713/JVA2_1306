35d78a7ca793c0daacb7ac9d95117996090bf696
Rueben Anderson <rueand713@fullsail.edu>
2013-06-04 15:38:20 -0500

    Finalized Code Transfer
    Finished transfering all of the files to the new project folder. Reset
    the manifest file permissions.

--- Project/gen/com/randerson/java2project/R.java
+++ Project/gen/com/randerson/java2project/R.java
@@ -21,21 +21,56 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int cloudy=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int rainy=0x7f020002;
+        public static final int sunny=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000f;
+        public static final int conditionheader=0x7f080000;
+        public static final int currentcondition=0x7f080002;
+        public static final int gridconditions=0x7f080001;
+        public static final int humiditycondition=0x7f080004;
+        public static final int instructions=0x7f08000c;
+        public static final int locales=0x7f08000d;
+        public static final int mainll=0x7f080007;
+        public static final int tempcondition=0x7f080003;
+        public static final int title=0x7f080008;
+        public static final int weather_btn=0x7f08000e;
+        public static final int weatherimage=0x7f080009;
+        public static final int weatherimage2=0x7f08000a;
+        public static final int weatherimage3=0x7f08000b;
+        public static final int windircondition=0x7f080006;
+        public static final int winspdcondition=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int details=0x7f030000;
+        public static final int radioform=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int apikey=0x7f050006;
         public static final int app_name=0x7f050000;
+        public static final int blank=0x7f050011;
+        public static final int buttontext=0x7f050005;
+        public static final int chicago=0x7f050008;
+        public static final int condition=0x7f05000b;
+        public static final int current=0x7f050012;
         public static final int hello_world=0x7f050002;
+        public static final int houston=0x7f050007;
+        public static final int humidity=0x7f05000c;
+        public static final int instructions=0x7f050003;
+        public static final int logo=0x7f050010;
+        public static final int miami=0x7f050009;
+        public static final int seattle=0x7f05000a;
+        public static final int temp=0x7f05000d;
+        public static final int titletext=0x7f050004;
+        public static final int windir=0x7f05000f;
+        public static final int winspd=0x7f05000e;
     }
     public static final class style {
         /** 
@@ -57,12 +92,16 @@ public final class R {
         AppBaseTheme from BOTH res/values/styles.xml and
         res/values-v11/styles.xml on API 14+ devices.
     
- API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int ImageStyle=0x7f060006;
+        public static final int RadioStyle=0x7f060005;
+        public static final int Titles=0x7f060002;
+        public static final int WeatherInfo=0x7f060004;
+        public static final int WeatherTitles=0x7f060003;
     }
 }
---
+++ Project/src/systemPack/JSONhandler.java
@@ -0,0 +1,46 @@
+package systemPack;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class JSONhandler {
+	public static String readJSONObject(String JSONString, String key)
+	{
+		// create a container string for the json value
+		String jsonValue = "";
+		
+		// builds and returns a JSON object from the string
+		JSONObject json = returnJSONObject(JSONString);
+		
+		try {
+			// set the JsonValue string by querying through the json object
+			jsonValue = json.getJSONObject("data").getJSONArray("current_condition").getJSONObject(0).getString(key);
+			
+			Log.i("JSON", jsonValue);
+		} catch (JSONException e) {
+			Log.e("ERROR", "JSON Exception error");
+		}
+		
+		return jsonValue;
+	}
+	
+	public static JSONObject returnJSONObject(String JSONString)
+	{
+		// create a null JSON object
+		JSONObject thisJSON = null;
+		
+		try {
+			
+			// create a new JSON object with the passed in JSON string
+			thisJSON = new JSONObject(JSONString);
+			
+		} catch (JSONException e) {
+			Log.e("ERROR", "JSON Exception Error");
+		}
+		
+		// return the object
+		return thisJSON;
+	}
+}
