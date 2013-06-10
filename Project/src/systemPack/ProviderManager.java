08a946cd279a903c41565a26f70168f030c66d03
Rueben Anderson <rueand713@fullsail.edu>
2013-06-10 16:05:45 -0500

    Added Initial Provider class
    First week commit is a class that will hold the methods for accessing
    the content provider data within the application.

---
+++ Project/src/systemPack/ProviderManager.java
@@ -0,0 +1,13 @@
+package systemPack;
+
+import android.content.ContentProvider;
+
+public class ProviderManager {
+
+	private ContentProvider provider;
+	
+	public ProviderManager() {
+	//	provider = ContentProvider();
+	}
+	
+}
