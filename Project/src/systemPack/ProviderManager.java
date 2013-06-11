2a3c46195832f6265e0ac2cdff0c8e772f1a2185
Rueben Anderson <rueand713@fullsail.edu>
2013-06-11 14:48:27 -0500

    Updated Provider
    Added required overridden methods for the content provider.

--- Project/src/systemPack/ProviderManager.java
+++ Project/src/systemPack/ProviderManager.java
@@ -1,13 +1,62 @@
 package systemPack;
 
 import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.BaseColumns;
 
-public class ProviderManager {
+public class ProviderManager extends ContentProvider {
 
-	private ContentProvider provider;
+	// create the provider AUTHORITY string
+	public static final String AUTHORITY = "com.randerson.java2project.providermanager";
 	
-	public ProviderManager() {
-	//	provider = ContentProvider();
+	public static class ProviderData implements BaseColumns
+	{
+		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/days");
+		
+		public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.randerson.java2project.item";
+		
 	}
+	
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		// throw error on provider calls for non supported operations
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// throw error on provider calls for non supported operations
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean onCreate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
 	
 }
